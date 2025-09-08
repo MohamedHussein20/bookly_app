a068e467423e500bcdf37f670c74379b30a65a31
Mohamed Hussein <mohussein1279@gmail.com>
2025-09-09 02:30:10 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
