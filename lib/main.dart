53b88277f8874b3def2cb025ab8d8ac6cc5a704d
Mohamed Hussein <mohussein1279@gmail.com>
2025-09-13 02:44:48 +0300

    modify project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +12,7 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
-      home: null,
+      home: SplashView(),
     );
   }
 }
