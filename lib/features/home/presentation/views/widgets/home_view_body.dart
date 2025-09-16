e8cdc2e011c9b1c824a80666c2134acefa6fed60
Mohamed Hussein <mohussein1279@gmail.com>
2025-09-16 19:42:37 +0300

    refactor code

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/featured_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -12,27 +12,6 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedBookListView(),
       ],
-    );
-  }
-}
-
-class FeaturedBookListView extends StatelessWidget {
-  const FeaturedBookListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: FeaturedItem(),
-          );
-        },
-        itemCount: 10,
-      ),
     );
   }
 }
