90e71565f6ff0ae809a6b21085ef9fca24938245
Mohamed Hussein <mohussein1279@gmail.com>
2025-09-16 23:23:16 +0300

    create app styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff150526);
 const Duration kTransitionDuration = Duration(milliseconds: 500);
+const kGtSectraFine = 'assets/fonts/GT-Sectra-Fine.ttf';
