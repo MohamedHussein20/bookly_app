26f2beab1df72508e4cae68c97417ba267bf0524
Mohamed Hussein <mohussein1279@gmail.com>
2025-09-18 22:06:30 +0300

    add featured item to book details view

--- lib/features/home/presentation/views/widgets/featured_item.dart
+++ lib/features/home/presentation/views/widgets/featured_item.dart
@@ -6,9 +6,9 @@ class FeaturedItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
+      height: MediaQuery.of(context).size.height * 0.3,
       child: AspectRatio(
-        aspectRatio: 5 / 8,
+        aspectRatio: 2.7 / 4,
         child: Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
