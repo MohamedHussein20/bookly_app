0bd86d642a7925b1c90c436669b2e5193229bc62
Mohamed Hussein <mohussein1279@gmail.com>
2025-09-20 03:50:49 +0300

    implement fetch featured books

--- lib/features/home/data/repos/home_repository_impl.dart
+++ lib/features/home/data/repos/home_repository_impl.dart
@@ -31,5 +31,21 @@ class HomeRepositoryImpl implements HomeRepository {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {}
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      final data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&q=subject:Programming",
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
